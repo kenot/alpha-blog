5d515915feba77bf3e543b90001a63307b8331c6
tdinkov <tdinkov@cisco.com>
2024-02-27 10:45:05 +0200

    Addded pagination to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,11 @@ class ArticlesController < ApplicationController
     end
 
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 5)
     end
 
     def new
-        @article = Article.new 
+        @article = Article.new
     end
 
     def edit
